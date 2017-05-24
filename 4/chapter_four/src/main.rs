bd7ed6c983341ad15fd9346bfe894be152ffbbce
koki natsume <natsume.pcuse75@gmail.com>
2017-05-24 16:23:51 +0900

    [Add]4.5

---
+++ 4/chapter_four/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+  let mut x = 5;
+
+  let y = if x==5 { 10 } else { 15 };
+
+  println!("{}",y);
+}
---
+++ 4/chapter_four/target/debug/chapter_four.d
@@ -0,0 +1,1 @@
+/Users/natsumekoki/myDevelop/rustBook/4/chapter_four/target/debug/chapter_four: /Users/natsumekoki/myDevelop/rustBook/4/chapter_four/src/main.rs
