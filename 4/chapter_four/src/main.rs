7da337bc8c3419b36dc262fe44353d6d536195df
koki natsume <natsume.pcuse75@gmail.com>
2017-05-25 17:19:39 +0900

    [Add] 4.13　マッチ

--- 4/chapter_four/src/main.rs
+++ 4/chapter_four/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
-  let mut x = 5;
+  let x = 5;
 
   let y = if x==5 { 10 } else { 15 };
 
   println!("{}",y);
+
+  let number = match x {
+      1 => "one",
+      3 => "three",
+      5 => "five",
+      _ => "another number",
+  };
+
+  println!("{}",number);
 }
